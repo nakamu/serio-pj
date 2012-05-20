0950499547270632597a47baff62cf03113b92d2
joseph.swinging <joseph.swinging@c275c5fa-685b-0b86-edad-3d20f0fe5755>
2012-05-20 15:22:47 +0000

    pwm テストコードを追加 serialはRS232Cのレベル変換ICがないと動かない。 割り込みはうまく動かなかったのでポーリングすることにした。

---
+++ mbed/samplecodes/pwmTest/Prompt.hpp
@@ -0,0 +1,24 @@
+#ifndef _PROMPT_HPP
+#define _PROMPT_HPP
+#include "mbed.h"
+#include "pwmParameter.hpp"
+
+extern Serial pc;
+extern pwmParameter ch1;
+extern pwmParameter ch2;
+
+class Prompt {
+public:
+    Prompt();
+    virtual ~Prompt() {}
+    void run();
+protected:
+    void  hello();
+    int   ask_ch();
+    float ask_duty();
+    float ask_period();
+    float ask_pulsewidth();
+    float get_float();
+};
+
+#endif

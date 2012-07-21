6441ddcca24c349f796a3ddb2a36b616da3660d3
joseph.swinging@gmail.com <joseph.swinging@gmail.com@c275c5fa-685b-0b86-edad-3d20f0fe5755>
2012-07-21 17:53:37 +0000

    Moved to hw

--- camera_debug/env/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-### Common settings ###
-export PROJECT=`pwd`
-
-### Hardware Development releated ###
-export RTL_DIR=$PROJECT/rtl
-export ISE_DIR=$PROJECT/ise
-export VERIF_DIR=$PROJECT/verif
